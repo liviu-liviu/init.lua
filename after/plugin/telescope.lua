a9c2695bf5e39a77f5a739604d889e27f5ce58c5
Liviu <liviu@liviu.io>
2023-08-15 15:45:53 +0000

    add some keymap descriptions

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,9 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = 'Find file'})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = 'Find git file'})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+end,
+{desc = 'Grep for string'})
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {desc = 'Help tags'})
 
